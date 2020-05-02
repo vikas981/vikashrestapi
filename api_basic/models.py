a0f2d9f7e4cf050776a7300736d32f7513a94d3e
vikas981 <vs98990@gmail.com>
2020-05-02 22:33:55 +0530

    it is better

--- api_basic/models.py
+++ api_basic/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 
 class Test(models.Model):
-    teststep = models.CharField(max_length=10)
+    TESTSTEP=(
+        ('TC_1', 'TC_1'),
+        ('TC_2', 'TC_2'),
+        ('TC_3', 'TC_3')
+    )
+    STATUS=(
+        ('200', '200'),
+        ('400', '400'),
+        ('404', '404')
+    )
+    teststep = models.CharField(max_length=10,choices=TESTSTEP)
     testcase = models.CharField(max_length=10)
     description = models.CharField(max_length=100)
     url = models.CharField(max_length=100, default='vikas.com')
-    status = models.IntegerField()
+    status = models.IntegerField(choices=STATUS)
     authkey = models.CharField(max_length=100)
 
     def __str__(self):
